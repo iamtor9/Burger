5c9bcb504e7b6726dfea5f24a3a6d9c55bec5207
iamtor9 <iamtor9@gmail.com>
2019-09-25 17:48:29 -0500

    update 2

---
+++ models/burger.js
@@ -0,0 +1,23 @@
+module.exports = function (sequelize, DataTypes) {
+    const Burger = sequelize.define("Burger", {
+    });
+    return Burger;
+ }
+
+const burger = sequelize.define("Post", {
+    burger_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    devoured: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+  });
+  return Post;

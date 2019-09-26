28c063b0b4feaebea800872243b9d0e19ae130b4
iamtor9 <iamtor9@gmail.com>
2019-09-26 16:55:34 -0500

    fixed error. hell yeahhh

--- models/burger.js
+++ models/burger.js
@@ -2,22 +2,23 @@ module.exports = function (sequelize, DataTypes) {
     const Burger = sequelize.define("Burger", {
     });
     return Burger;
+
+    const burger = sequelize.define("Post", {
+        burger_name: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          validate: {
+            len: [1]
+          }
+        },
+        devoured: {
+          type: DataTypes.TEXT,
+          allowNull: false,
+          validate: {
+            len: [1]
+          }
+        },
+      });
+      return Post;
  }
 
-const burger = sequelize.define("Post", {
-    burger_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1]
-      }
-    },
-    devoured: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-      validate: {
-        len: [1]
-      }
-    },
-  });
-  return Post;
--- models/index.js
+++ models/index.js
@@ -1,14 +1,15 @@
-const fs = require("fs");
-const path = require("path");
-const Sequelize = require("sequelize");
-const basename = path.basename(module.filename);
-const env = process.env.NODE_ENV || "development";
-const config = require(__dirname + "/../config/babyT.json")[env];
-const db = {};
+var fs = require("fs");
+var path = require("path");
+var Sequelize = require("sequelize");
+var basename = path.basename(module.filename);
+var env = process.env.NODE_ENV || "development";
+var config = require(__dirname + "/../config/babyT.json")[env];
+var db = {};
+
 if (config.use_env_variable) {
- const sequelize = new Sequelize(process.env[config.use_env_variable]);
+ var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
- const sequelize = new Sequelize(config.database, config.username, config.password, config);
+ var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 fs
  .readdirSync(__dirname)

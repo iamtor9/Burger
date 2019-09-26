69c7a2c76d18e82572e7696e7b23e7ee99837ec4
iamtor9 <iamtor9@gmail.com>
2019-09-26 16:28:53 -0500

    sql update

--- db/schema.sql
+++ db/schema.sql
@@ -3,11 +3,13 @@ CREATE DATABASE burger_db;
 USE burger_db;
 
 
-CREATE TABLE burgers (
-    id INT NOT NULL AUTO_INCREMENT,
-    burger_name VARCHAR(50) NULL,
-    devoured BOOLEAN,
-    PRIMARY KEY: id
+-- CREATE TABLE burgers (
+--     id INT NOT NULL AUTO_INCREMENT,
+--     burger_name VARCHAR(50) NULL,
+--     devoured BOOLEAN,
+--     PRIMARY KEY: id
 
-)
+-- )
 
+ALTER TABLE burgers CHANGE COLUMN createdAt createdAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE burgers CHANGE COLUMN updatedAt updatedAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP;

7b1b826562706e033f1410322d203dd016d7ed4d
iamtor9 <iamtor9@gmail.com>
2019-09-26 15:31:15 -0500

    uopfate

---
+++ server.js
@@ -0,0 +1,31 @@
+//importing node package express
+const express = require("express");
+//const  = require("express-handlebars");
+
+//app is going to use express package
+const app = express();
+const PORT = process.env.PORT || 4200;
+
+// Requiring our models for syncing
+const db = require("./models");
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static directory
+app.use(express.static("public"));
+
+// Routes
+// =============================================================
+// require("./routes/api-routes.js")(app);
+// require("./routes/html-routes.js")(app);
+
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
+

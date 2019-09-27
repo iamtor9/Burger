21b116a25f016b4e269c245fa6c073a039db72db
iamtor9 <iamtor9@gmail.com>
2019-09-27 00:28:48 -0500

    "I won't give up on us, even when the code gets tough..."

--- db/seed.sql
+++ db/seed.sql
@@ -1,15 +1,36 @@
-INSERT INTO burgers (
-    "burger_name",
-    "devoured",
-)
+--third time is a charm?
+-- use the burger db!!!!!! duh.
+-- insert into burgers, then input values
+USE burger_db;
 
--- referenced other ways to create seeds sql, between google and recent assignments
--- and messing around until it clicked. so hoping this works!
-VALUES
-("juicy lucy", false),
-("bae burger", false),
-("bomb burger", false),
-("boo burger", false);
+INSERT INTO burgers (name, devoured, createdAt, updatedAt) 
+VALUES ("bae burger", false, CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
+
+INSERT INTO burgers (name, devoured, createdAt, updatedAt) 
+VALUES ("juicy lucy", false, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+INSERT INTO burgers (name, devoured, createdAt, updatedAt) 
+VALUES ("Bomb burger", true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+INSERT INTO burgers (name, devoured, createdAt, updatedAt) 
+VALUES ("spooky boo burger", false, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+INSERT INTO burgers (name, devoured, createdAt, updatedAt) 
+VALUES ("baller burger", true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+
+--second code attmpt
+-- INSERT INTO burgers (
+--     "burger_name",
+--     "devoured",
+-- )
+-- -- referenced other ways to create seeds sql, between google and recent assignments
+-- -- and messing around until it clicked. so hoping this works!
+-- VALUES
+-- ("juicy lucy", false),
+-- ("bae burger", false),
+-- ("bomb burger", false),
+-- ("boo burger", false);
 
 
 -- first code 
